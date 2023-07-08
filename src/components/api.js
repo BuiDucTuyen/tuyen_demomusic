4e34cb438e03e89fc23e578fa07d9e3c87417c24
BuiDucTuyen <buiductuyengl1072002@gmail.com>
2023-07-08 07:29:01 +0700

    update

---
+++ src/components/api.js
@@ -0,0 +1,24 @@
+// import axios from "axios";
+// import React, { useEffect } from "react";
+
+// const fetchData = async () => {
+//     try {
+//       const response = await axios.get("https://example.com/api/songs");
+//       const songs = response.data;
+
+//       console.log(songs);
+//     } catch (error) {
+
+//       console.error(error);
+//     }
+//   };
+
+// function App() {
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+// }
+
+// export default App;
